92fec0c47f43a21f75ebec978e824bf3b8dc26ba
JohnConstantion <1906172933@qq.com>
2020-11-30 12:28:51 +0800

    Initial commit

---
+++ src/main/java/com/john/garden/GardenApplication.java
@@ -0,0 +1,16 @@
+package com.john.garden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**  初始化秘密花园项目
+ * @author constantinejohn
+ */
+@SpringBootApplication
+public class GardenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/john/garden/GardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.garden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
